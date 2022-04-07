4ed85e096c6650fa226e2c68d5b9ccd667072bbe
AbhiPj <parajuli.abhinav.ap@gmail.com>
2022-04-07 21:21:54 +0545

    Project setup

---
+++ RopeyDVD/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RopeyDVD.Models;
+
+namespace RopeyDVD.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+
+        }
+
+    }
+}
